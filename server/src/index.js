1c27e2876a4f1aebcf95743844a0ca90ced08034
Lalit Kumar Sharma <lalitkumarsharma.jsr@gmail.com>
2023-03-03 17:52:20 +0530

    Established API

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,12 @@
+const { response } = require("express");
 const express = require("express");
+const OPENAI_API_KEY = "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"
+const { Configuration, OpenAIApi } = require ("openai");
+const configuration = new Configuration({
+     apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const app = express();
 
 app.use(express.json());
@@ -11,11 +19,32 @@ app.get("/ping", (req, res) => {
 
 app.get("/chat", (req, res) => {
   const question = req.body.question;
-  console.log({question});
+
+  openai
+.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  frequency_penalty: 0.5,
+  // max_tokens: 100,
+  user: "Admission Seeker or Students of Arka Jain University",
+  messages: [
+    // {role: "user", content: "Answer as if your are a chatbot named 'AISHA' that stands for 'Artificially Intelligent Super Helpful Assistant' and is designed to assist student and admission seeker of Arka Jain University by a group of students who named themselves AI Forge. The website for the university is 'arkajainuniversity.ac.in'"}, 
+    {role: "user", content : question}
+],
+}).then(response => {
+  return(response.data.choices[0].message);
+})
+// .then((answer) => {
+//   const array = answer?.split("\n").filter((value) => value).map(value => value.trim());
+// })
+.then(answer => {
   res.json({
-    answer: "pong",
-    question,
+    answer: answer,
+    prompt : question,
   });
+});
+
+  console.log({question});
+  
 });
 
 app.listen(3000, () => {
